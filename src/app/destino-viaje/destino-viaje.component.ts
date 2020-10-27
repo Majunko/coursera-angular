50b05f9138e7903e5867fca711d5a5c9fa5e8be0
Ferney Palacio <themistico9000@gmail.com>
2020-10-26 23:25:37 -0500

    Tarea Semana 1 Completa.

---
+++ src/app/destino-viaje/destino-viaje.component.ts
@@ -0,0 +1,19 @@
+import { Component, HostBinding, Input, OnInit } from '@angular/core';
+import { DestinoViaje } from '../models/destino-viaje.model';
+
+@Component({
+  selector: 'app-destino-viaje',
+  templateUrl: './destino-viaje.component.html',
+  styleUrls: ['./destino-viaje.component.css']
+})
+export class DestinoViajeComponent implements OnInit {
+
+  @Input() destino: DestinoViaje;
+  @HostBinding('attr.class') cssClass = 'col-md-4 mb-4';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
